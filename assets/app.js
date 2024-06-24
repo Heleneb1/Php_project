eaefd04ba2475e3139d19e0af109035b4f2be36e
HeleneB <helene.blanche@hotmail.fr>
2024-06-24 09:33:41 +0200

    Learn Symfony

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,10 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+// assets/app.js
+import './styles/global.scss';
+import 'bootstrap';
+
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,27 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Attribute\Route;
+
+Class ProgramController extends AbstractController
+{
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+    #[Route('/program/{id}', methods: ['GET'], name: 'program_show')]
+    public  function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+    }
+    
+}
